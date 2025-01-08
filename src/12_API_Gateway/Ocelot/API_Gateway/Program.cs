4b70fd2a92a323bc23500670a04447a89be7b5d9
Berkay Zaim <berkayzaimdev@gmail.com>
2025-01-09 01:55:07 +0300

    added load balancing to ocelot and yarp gateways

--- src/12_API_Gateway/Ocelot/API_Gateway/Program.cs
+++ src/12_API_Gateway/Ocelot/API_Gateway/Program.cs
@@ -10,28 +10,28 @@ builder.Configuration.AddJsonFile("ocelot.json");
 
 builder.Services.AddOcelot(builder.Configuration);
 
-builder.Services.AddAuthorization();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
-    {
-        opts.TokenValidationParameters = new()
-        {
-            ValidateAudience = true,
-            ValidateIssuer = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Token:Issuer"],
-            ValidAudience = builder.Configuration["Token:Audience"],
-            IssuerSigningKey =
-                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
-            ClockSkew = TimeSpan.Zero
-        };
-    });
+// builder.Services.AddAuthorization();
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opts =>
+//     {
+//         opts.TokenValidationParameters = new()
+//         {
+//             ValidateAudience = true,
+//             ValidateIssuer = true,
+//             ValidateLifetime = true,
+//             ValidateIssuerSigningKey = true,
+//             ValidIssuer = builder.Configuration["Token:Issuer"],
+//             ValidAudience = builder.Configuration["Token:Audience"],
+//             IssuerSigningKey =
+//                 new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
+//             ClockSkew = TimeSpan.Zero
+//         };
+//     });
 
 var app = builder.Build();
 
-app.UseAuthentication();
-app.UseAuthorization();
+// app.UseAuthentication();
+// app.UseAuthorization();
 
 await app.UseOcelot();
 app.UseHttpsRedirection();
